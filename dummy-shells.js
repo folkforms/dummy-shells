4a6e6194a3e7da35b589777386ab7de14a9f060a
folkforms <alan.bergin@gmail.com>
2021-07-09 23:02:24 +0100

    Added dummy shells

---
+++ dummy-shells.js
@@ -0,0 +1,9 @@
+const dummyShellJs = require("./shells/dummyShellJs");
+const dryRunShellJs = require("./shells/dryRunShellJs");
+const failingShellJs = require("./shells/failingShellJs");
+
+module.exports = {
+  dummyShellJs,
+  dryRunShellJs,
+  failingShellJs,
+};
